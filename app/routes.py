1014b963faf17752a51fc0b87d4149c489751490
Sukleta Dima <dima.sukleta03@gmail.com>
2023-03-18 15:26:29 +0200

    Task-10, commit 2.2

--- app/routes.py
+++ app/routes.py
@@ -7,31 +7,16 @@ from app.orm_querys import get_groups_with_less_students_count, \
     add_student_to_course, delete_student_from_course
 
 
-class GroupsLessThenCount(Resource):
+class Student(Resource):
     def get(self):
-        max_student_in_group = request.args.get('student_count')
-        result = get_groups_with_less_students_count(max_student_in_group)
-        data = [{'group_name': row[0], 'student_count': row[1]} for row in result]
-        return data
-
-
-class StudentsFromCourse(Resource):
-    def get(self):
+        # get students by there course
         course_name = request.args.get('course_name')
         result = students_from_course_by_name(course_name)
         data = [{'first_name': row[0], 'last_name': row[1], 'course_name': row[2]} for row in result]
         return data
 
-
-class DeleteStudent(Resource):
-    def get(self):
-        student_id = request.args.get('student_id')
-        result = delete_student_by_id(student_id)
-        return {result: 'Was deleted'}
-
-
-class AddStudent(Resource):
-    def get(self):
+    def put(self):
+        # new student
         student_id = request.args.get('student_id')
         first_name = request.args.get('first_name')
         last_name = request.args.get('last_name')
@@ -42,17 +27,29 @@ class AddStudent(Resource):
         except:
             return {'new_student': 'Is already exist or validation failed'}
 
+    def delete(self):
+        # delete student by id
+        student_id = request.args.get('student_id')
+        result = delete_student_by_id(student_id)
+        return {result: 'Was deleted'}
 
-class AddStudentToCourse(Resource):
+
+class Group(Resource):
     def get(self):
+        max_student_in_group = request.args.get('student_count')
+        result = get_groups_with_less_students_count(max_student_in_group)
+        data = [{'group_name': row[0], 'student_count': row[1]} for row in result]
+        return data
+
+
+class Course(Resource):
+    def put(self):
         student_id = request.args.get('student_id')
         course_name = request.args.get('course_name').split(' ')
         result = add_student_to_course(student_id, course_name)
         return {result[0]: [course for course in result[1]]}
 
-
-class DeleteFromCourse(Resource):
-    def get(self):
+    def delete(self):
         from app.orm_querys import session
 
         student_id = request.args.get('student_id')
@@ -62,10 +59,7 @@ class DeleteFromCourse(Resource):
 
 
 api = Api()
-api.add_resource(GroupsLessThenCount, '/groups')
-api.add_resource(StudentsFromCourse, '/students')
-api.add_resource(DeleteStudent, '/delete')
-api.add_resource(AddStudent, '/add')
-api.add_resource(AddStudentToCourse, '/add_to_course')
-api.add_resource(DeleteFromCourse, '/delete_from_course')
+api.add_resource(Student, '/api/v1/student')
+api.add_resource(Group, '/api/v1/group')
+api.add_resource(Course, '/api/v1/course')
 
